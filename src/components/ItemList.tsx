a245ca52d75e30c0723c2c6ca58175d428bdc455
Diego Soto <leonardo.soto123@hotmail.com>
2023-10-17 14:29:26 -0300

    fix list icon

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -5,7 +5,7 @@ type Props = {
 const ItemList = ({ text }: Props) => {
   return (
     <div className="flex gap-4 items-start">
-      <img src="../public/assets/images/icon-list.svg" alt="" />
+      <img src="./assets/images/icon-list.svg" alt="" />
       <li>{text}</li>
     </div>
   )
